eb5df0c54a30857984fb0d601511c9a86aa1a101
truongminhquang <quangff296@gmail.com>
2024-11-27 14:35:34 +0700

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "MQ",
+    age: 20,
+    address: "QN",
+  };
+
+  handleClick(event) {
+    // console.log(">>Click me");
+    console.log(event);
+  }
   //jSX
   render() {
-    return <div>my first Component</div>;
+    return (
+      <div>
+        My name is {this.state.name} , I am {this.state.age} years old , I live
+        in {this.state.address}
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
   }
 }
 
